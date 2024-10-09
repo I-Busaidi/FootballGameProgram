4ea50a1728188b347a980006ef04532b712fd991
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-10-09 12:02:26 +0400

    Added attributes and methods to the abstract class "Player".

--- Player.cs
+++ Player.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace FootballGameProgram
 {
-    public class Player
+    public abstract class Player
     {
+        public int PlayerNumber { get; private set; }
+        public string Name { get; private set; }
+        public int PowerLevel { get; private set; }
+        public enum Position
+        {
+            Forward, MidFielder, Defender, GoalKeeper
+        }
+        public Position position { get; private set; }
+        public Team team { get; private set; }
+
+        public Player(int playerNumber, string name, int powerLevel, Position position, Team team)
+        {
+            PlayerNumber = playerNumber;
+            Name = name;
+            PowerLevel = powerLevel;
+            this.position = position;
+            this.team = team;
+        }
+
+        public abstract void GetPlayerInfo();
     }
 }
